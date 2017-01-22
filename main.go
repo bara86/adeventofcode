ccc952d7408c573b9a7e1536eec7bf831b37003c
Bara <bara@develer.com>
2017-01-22 12:19:04 +0100

    Add day 3 part 2 solution

--- main.go
+++ main.go
@@ -8,38 +8,50 @@ import (
 	"strconv"
 )
 
-func checkTriangle(els []int) bool {
+func checkTriangle(numbers ...string) bool {
+	var els []int
+	for _, x := range numbers {
+		v, _ := strconv.Atoi(x)
+		els = append(els, v)
+	}
+
 	return els[0]+els[1] > els[2] && els[0]+els[2] > els[1] && els[1]+els[2] > els[0]
 }
 
-func part1() {
+func openFile() *os.File {
 	file, err := os.Open("numbers.txt")
-	defer file.Close()
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
+	}
+	return file
+}
+
+func createRegexp() *regexp.Regexp {
+	reg, err := regexp.Compile("([0-9]+)")
+	if err != nil {
+		panic(err)
 	}
 
+	return reg
+}
+
+func getLineElements(line string, reg *regexp.Regexp) []string {
+	return reg.FindAllString(line, -1)
+}
+
+func part1() {
+	file := openFile()
+	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
-	reg, e := regexp.Compile("([0-9]+)")
-	if e != nil {
-		fmt.Println(e)
-		return
-	}
+	reg := createRegexp()
 
 	count := 0
 
 	for scanner.Scan() {
-		line := scanner.Text()
-
-		var els []int
-		for _, x := range reg.FindAllString(line, -1) {
-			v, _ := strconv.Atoi(x)
-			els = append(els, v)
-		}
-
-		if checkTriangle(els) {
+		numbers := getLineElements(scanner.Text(), reg)
+		if checkTriangle(numbers...) {
 			count++
 		}
 	}
@@ -47,6 +59,32 @@ func part1() {
 	fmt.Println("count part 1", count)
 }
 
+func part2() {
+	file := openFile()
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	reg := createRegexp()
+
+	count := 0
+	var numbers []string
+
+	for scanner.Scan() {
+		numbers = append(numbers, getLineElements(scanner.Text(), reg)...)
+	}
+
+	for i := 0; i < len(numbers)/3; i += 3 {
+		for j := 0; j < 3; j++ {
+			if checkTriangle(numbers[i*3+j], numbers[i*3+(j+3)], numbers[i*3+(j+6)]) {
+				count++
+			}
+		}
+	}
+
+	fmt.Println("Count part 2", count)
+}
+
 func main() {
 	part1()
+	part2()
 }
